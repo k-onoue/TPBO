058557425a06129d172d2dc962b078916879188e
k-onoue <k.onoue0724@gmail.com>
2024-09-18 21:47:45 +0900

    fix comment

--- src/utils_experiment.py
+++ src/utils_experiment.py
@@ -5,7 +5,7 @@ import sys
 
 
 def set_logger(log_filename_base, save_dir):
-    # ログの設定
+    # Set up logging
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_filename = f"{current_time}_{log_filename_base}.log"
     log_filepath = os.path.join(save_dir, log_filename)
